24d377f5c6fc72c70dd27c79c45faba27ed4d7da
Storm00212 <paulmuyalikhams@gmail.com>
2026-01-09 15:19:10 +0300

    d ff

---
+++ mealy_d.v
@@ -0,0 +1,19 @@
+module mealy_d (
+    input clk,
+    input reset,
+    input x,
+    output reg z
+);
+    reg q;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            q <= 1'b0;
+        else
+            q <= q ^ x;
+    end
+
+    always @(*) begin
+        z = q ^ x;
+    end
+endmodule
